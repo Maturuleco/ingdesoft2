092cafd186ecfa9cf38d9308c570ec89daae9c6a
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-25 17:48:10 +0000

    - Subo el predictor, nueva version flexible al ingreso de reglas

---
+++ TP/Prueba de concepto/Modelo/src/predictorModel/Comparador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package predictorModel;
+
+/**
+ *
+ * @author Santiago Avendaño
+ */
+public enum Comparador {
+    menor,
+    mayor,
+    igual,
+    mayeq,
+    meneq,
+}
--- TP/Prueba de concepto/predictor/src/predictor/Predictor.java
+++ TP/Prueba de concepto/predictor/src/predictor/Predictor.java
@@ -6,31 +6,74 @@
 package predictor;
 
 import java.util.Collection;
+import predictorModel.Regla;
+import java.util.EnumMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import model.Alerta;
 import model.DatoSensado;
+import model.FactorClimatico;
 
 /**
  *
  * @author Santiago Avendaño
  */
-public abstract class Predictor {    
+public class Predictor {
 
-    public List<Alerta> analizar(Map<Integer, Collection<DatoSensado>> datosSensados){
-        Set<Integer> idTRs = datosSensados.keySet();
-        List<Alerta> alertas = new LinkedList<Alerta>();
-        for (Integer id : idTRs) {
-            if (analizar(datosSensados.get(id))) {
-                alertas.add(new Alerta(id, "Se detectó un huracán"));
+
+    // Analiza que todos los datos cumplan todas las reglas
+    // Controlando todos con todos
+    public Boolean analizar(Collection<Regla> reglas, Collection<DatoSensado> datos){
+        for (Regla regla : reglas) {
+            for (DatoSensado dato  : datos) {
+                if (!regla.aplicar(dato)) return Boolean.FALSE;
             }
         }
-        return alertas;
+        return Boolean.TRUE;
+    }
+    
+    // Analiza que el dato cumpla todas las reglas
+    public Boolean analizar(Collection<Regla> reglas, DatoSensado dato){
+        for (Regla regla : reglas) {
+            if (!regla.aplicar(dato)) return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
     }
 
-    public abstract Boolean analizar(Collection<DatoSensado> datosAEvaluar);
+    // Analiza q la regla se satisfaga en todos los datos
+     public Boolean analizar(Regla regla, Collection<DatoSensado> datos){
+         for (DatoSensado dato : datos) {
+             if (!regla.aplicar(dato)) return Boolean.FALSE;
+         }
+         return Boolean.TRUE;
+    }
+
+    // Analiza que todos los datos cumplan todas las reglas
+    // Controlando por factor
+    public Boolean analizarReglasPorFactor(Collection<Regla> reglas, Collection<DatoSensado> datos){
+        Map<FactorClimatico, Collection<Regla>> reglasPorFactor;
+
+        reglasPorFactor = ordenarPorFactor(reglas);
+
+        for (DatoSensado dato: datos) {
+            if (analizar(reglasPorFactor.get(dato.getFactor()), dato)) return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+
+    public Map<FactorClimatico, Collection<Regla>> ordenarPorFactor(Collection<Regla> reglas){
+        Map<FactorClimatico, Collection<Regla>> result = new EnumMap<FactorClimatico, Collection<Regla>>(FactorClimatico.class);
+        
+        for (FactorClimatico factor : FactorClimatico.values()) {
+            result.put(factor, new LinkedList<Regla>() );
+        }
+
+        for (Regla regla : reglas) {
+            result.get(regla.getFactor()).add(regla);
+        }
+
+        return result;
+    }
+
 
 
 }
