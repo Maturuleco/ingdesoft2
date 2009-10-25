9508d8b9cf873850713cef57f3d262f84ad5ded9
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-25 16:17:07 +0000

    modificaciones a las clases del modelo, mas que nada los .parse y toString(). Y en constructor en Mensaje a partir de un MensajeGSM

--- TP/Prueba de concepto/Modelo/src/model/DataSource.java
+++ TP/Prueba de concepto/Modelo/src/model/DataSource.java
@@ -5,6 +5,8 @@
 
 package model;
 
+import java.text.ParseException;
+
 /**
  *
  * @author Santiago Avendaño
@@ -14,4 +16,33 @@ public enum DataSource {
     biggest_satellite,
     triangulador,
     sistema_eolico,
+    estacion_central;
+
+    public static DataSource parse(String id) throws ParseException{
+        DataSource res;
+        if (id.equals("T"))
+            res = DataSource.terminal_remota;
+        else if (id.equals("B"))
+            res = DataSource.biggest_satellite;
+        else if (id.equals("V"))
+            res = DataSource.triangulador;
+        else if (id.equals("E"))
+            res = DataSource.sistema_eolico;
+        else if (id.equals("C"))
+            res = DataSource.estacion_central;
+        else
+            throw new ParseException(id, 0);
+        return res;
+    }
+
+    @Override public String toString(){
+        switch(this) {
+            case terminal_remota: return "T";
+            case biggest_satellite: return "B";
+            case triangulador: return "V";
+            case sistema_eolico: return "E";
+            case estacion_central: return "C";
+            default: return "ERROR";
+        }
+    }
 }
--- TP/Prueba de concepto/Modelo/src/model/FactorClimatico.java
+++ TP/Prueba de concepto/Modelo/src/model/FactorClimatico.java
@@ -5,6 +5,8 @@
 
 package model;
 
+import java.text.ParseException;
+
 /**
  *
  * @author Santiago Avendaño
@@ -13,7 +15,39 @@ public enum FactorClimatico {
     temperatura,
     humedad,
     presion,
-    precipitaciones,
+    lluvias,
     velocidad_viento,
-    direccion_viento,
+    direccion_viento;
+
+    public static FactorClimatico parse(String id) throws ParseException{
+        FactorClimatico res;
+        if (id.equals("T"))
+            res = FactorClimatico.temperatura;
+        else if (id.equals("H"))
+            res = FactorClimatico.humedad;
+        else if (id.equals("P"))
+            res = FactorClimatico.presion;
+        else if (id.equals("L"))
+            res = FactorClimatico.lluvias;
+        else if (id.equals("V"))
+            res = FactorClimatico.velocidad_viento;
+        else if (id.equals("D"))
+            res = FactorClimatico.direccion_viento;
+        else
+            throw new ParseException(id, 0);
+        return res;
+    }
+
+    @Override public String toString(){
+        switch(this) {
+            case temperatura: return "T";
+            case humedad: return "H";
+            case presion: return "P";
+            case lluvias: return "L";
+            case velocidad_viento: return "V";
+            case direccion_viento: return "D";
+            default: return "ERROR";
+        }
+    }
+
 }
