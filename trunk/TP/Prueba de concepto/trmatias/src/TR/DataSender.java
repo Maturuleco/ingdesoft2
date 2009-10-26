d8c51708dd9b4b333a6c892f2ef978ff919e75e2
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-26 20:30:45 +0000

    Algo del data sender... Falta hacer que lea las dos entradas que tiene y no se trabe

--- trunk/TP/Prueba de concepto/trmatias/src/TR/DataSender.java
+++ trunk/TP/Prueba de concepto/trmatias/src/TR/DataSender.java
@@ -5,37 +5,73 @@
 
 package TR;
 
-import java.util.List;
-
 import red_gsm.MensajeGSM;
-import red_gsm.ModemGSM;
 import model.Mensaje;
+import red_gsm.MensajeModemGSM;
+import threadcomunication.ColaSync;
 
 /**
  *
  * @author Administrador
  */
-public class DataSender {
-    private ModemGSM modem;
-    private int estacionCentral = 0;
-    private List<Integer> enviados;
 
-    public void send(Mensaje m)
-    {
-        //Acá se haría la particion de los msjs
-        int id = m.getIdTR();
-        modem.send(estacionCentral, m.toString());
-        enviados.add(id);
+public class DataSender extends Thread {
+    private static final int estacionCentral = 0;
+    private static final int tamañoBuffer = 10;
+    private int estadoBuffer = 0;
+    private Sender[] enviando;
+    private Mensaje[] mensajesPendientes;
+    private ColaSync<Mensaje> salida;
+    private ColaSync<Mensaje> entrada;
+    private ColaSync<MensajeModemGSM> modemSalida;
+    private ColaSync<MensajeModemGSM> modemEntrada;
+    
+    public DataSender(ColaSync<Mensaje> e, ColaSync<Mensaje> s, ColaSync<MensajeModemGSM> m) {
+        enviando = new Sender[tamañoBuffer];
+        salida = s;
+        entrada = e;
+        modemSalida = m;
+    }
+    
+    public void run() {
+        
     }
 
-    public Boolean recive(MensajeGSM msj)
+    private void send(Mensaje m, int id)
     {
+        String mensaje = m.toString();
+        // Le pongo un Id al Mensaje en su cuerpo
+        mensaje = id+"#"+mensaje;
+        Sender s = new Sender(mensaje, modemSalida);
+        enviando[id] = s;
+        mensajesPendientes[id] = m;
+        estadoBuffer++;
+        s.start();
+    }
+
+    private Boolean recive(MensajeGSM msj) {
         if (msj.getOrigen() != estacionCentral)
             return false;
         else
         {
-            Mensaje respuesta = new Mensaje(MensajeGSM);
-            
+            String contenido = new String(msj.getMensaje());
+            //Saco el idMsj
+            String[] cuerpo = contenido.split("#");
+            if ( cuerpo[0].equals("CONFIG") )
+                manejarMensajeConfiguracion(msj);
+            else if ( cuerpo[0].equals("ACK") ) {
+                int id = Integer.valueOf(cuerpo[1]);
+                enviando[id].requestStop();
+                enviando[id] = null;
+                salida.put(mensajesPendientes[id]);
+                estadoBuffer--;
+            }
+            else
+                return false;
         }
+        return true;
     }
+
+    private void manejarMensajeConfiguracion(MensajeGSM msj) {}
+    
 }
