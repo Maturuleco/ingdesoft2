d56c026725abefcbcffe3a9845a46df2f05ed168
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-27 21:04:48 +0000

    Terminando de cerrar la redGSM antes de ir a Redes -> Ergo, no la testié

---
+++ TP/Prueba de concepto/red_gsm/src/red_gsm/ModemReciver.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package red_gsm;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tas
+ */
+public class ModemReciver extends Thread {
+
+    private static final long timeToWait = 100;
+    private File folderPropia;
+    private BlockingQueue<MensajeGSM> salida;
+
+    public ModemReciver(File folderPropia) {
+        this.folderPropia = folderPropia;
+    }
+
+    public void setSalida(BlockingQueue<MensajeGSM> salida) {
+        this.salida = salida;
+    }
+
+    private void recive() {
+        try {
+            File[] files = folderPropia.listFiles();
+            for (int j = 0; j < files.length; j++) {
+                File file = files[j];
+                //System.out.println( getName()+ "D:" + file.getName());
+                if(file.canRead()){
+                    FileReader fr = new FileReader(file);
+                    BufferedReader br = new BufferedReader(fr);
+                    String texto = br.readLine();
+                    // fr.close();
+                    file.delete();
+                    
+                    MensajeGSM mensaje = MensajeGSM.parse(texto);
+                    salida.put(mensaje);
+                }
+
+            }
+        } catch (Exception e) {
+              e.printStackTrace(System.err);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            recive();
+            try {
+                sleep(timeToWait);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ModemReciver.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+
+}
--- TP/Prueba de concepto/red_gsm/src/red_gsm/RedGSM.java
+++ TP/Prueba de concepto/red_gsm/src/red_gsm/RedGSM.java
@@ -10,15 +10,14 @@ import java.util.Hashtable;
 import java.util.Date;
 
 public class RedGSM {
-    private int numModem;
+    private static final String path = "GSM";
     private Dictionary< Integer , File > modems;
     private File carpeta;
     private static RedGSM instancia = null;
 	
     private RedGSM() {
         modems = new Hashtable< Integer, File >();
-        numModem = 1;
-        carpeta = new File("GSM");
+        carpeta = new File(path);
         carpeta.mkdir();
         modems.put(0, carpeta);
     }
@@ -29,47 +28,5 @@ public class RedGSM {
         return instancia;
     }
 	
-    public ModemGSM createModem()
-    {
-	File dir = new File(String.valueOf(numModem));
-	dir.mkdir();
-	modems.put(numModem, dir);
-	numModem++;
-	ModemGSM modem = new ModemGSM(numModem);
-	return modem;
-    }
-	
-    public void send(MensajeGSM msj)
-    {
-        File carpetaDst = modems.get(msj.getDestino());
-	String nombre = carpetaDst.getName() + "/" ;
-	Date fechaMsj = msj.getFecha();
-	
-	//La idea es que el nombre no esté repetido...
-	//String nombre = msj.getOrigen().toString() + "-" + fechaMsj.hashCode();
-	nombre += msj.getOrigen().toString() + "-" + fechaMsj.getTime();
-	nombre += ".txt";
-
-	try {
-            /*
-            File mensaje = new File(nombre);
-            FileWriter archivo = new FileWriter(mensaje);
-            */
-            FileWriter fstream;
-            fstream = new FileWriter(nombre);
-            BufferedWriter out = new BufferedWriter(fstream);
-		
-            out.write("De: "+msj.getOrigen().toString());
-            out.write("A: "+msj.getDestino().toString());
-	    //No le gusta lo de Date porque dice que está fuera de uso...
-//	    String fechaGMT = fechaMsj.toGMTString();
-//            out.write("Fecha: " + fechaGMT );
-	    out.write(msj.getMensaje());
-		    
-	    out.close();
-	} catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-	}		
-    }
+   
 }
