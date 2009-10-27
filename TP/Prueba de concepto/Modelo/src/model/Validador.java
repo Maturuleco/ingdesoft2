9572b8827674b1fbf770c6f9e285919aa75f478a
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-27 15:53:41 +0000

    Agrego un Validador en el modelo. Agrega el time Stamp en el mensaje.

---
+++ TP/Prueba de concepto/Modelo/src/model/Validador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author tas
+ */
+public class Validador {
+    
+    public static Boolean validar(String mensaje, DataSource origen) {
+        // TODO: hacer el cuerpo del validador de mensajes
+        return true;
+    }
+    
+}
