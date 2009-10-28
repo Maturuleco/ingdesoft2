dfee99ee33c61d1eb9e6a28f9e48270086380d8d
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 19:48:20 +0000

    - predictor implementado con threads

---
+++ trunk/TP/Prueba de concepto/predictor/src/predictor/PredictorManager.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package predictor;
+
+import java.util.Collection;
+import model.DatoAlmacenado;
+import modelo.Regla;
+import selectorDatos.SelectorDatos;
+
+/**
+ *
+ * @author Santiago Avendaño
+ */
+public class PredictorManager implements Runnable {
+
+    private static final long tiempoEspera = 10000;
+
+    private volatile boolean keepTrying = true;
+
+    private SelectorDatos selectorDatos;
+    private SelectorReglas selectorReglas;
+
+    public PredictorManager(SelectorDatos selectorDatos, SelectorReglas selectorReglas) {
+        this.selectorDatos = selectorDatos;
+        this.selectorReglas = selectorReglas;
+    }
+
+    @Override
+    public void run() {
+        while (keepTrying) {
+            try {
+                Collection<DatoAlmacenado> datos = selectorDatos.leerTodosLosDatos();
+                Collection<Regla> reglas = selectorReglas.getReglas();
+                Predictor predictor;
+                for (Regla regla : reglas) {
+                     predictor = new Predictor(regla, datos);
+                     new Thread(predictor).run();
+                }
+                Thread.sleep(tiempoEspera);
+            } catch (InterruptedException ex) { }
+        }
+    }
+
+    public void requestStop(){
+        keepTrying = false;
+    }
+
+}
--- trunk/TP/Prueba de concepto/predictor/test/predictorTest/SelectorDatosTest.java
+++ trunk/TP/Prueba de concepto/predictor/test/predictorTest/SelectorDatosTest.java
@@ -6,6 +6,7 @@ package predictorTest;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectServer;
 import java.io.File;
 import java.util.Calendar;
 import java.util.List;
@@ -52,17 +53,10 @@ public class SelectorDatosTest {
         DatoSensado dato1 = new DatoSensado(1, Calendar.getInstance().getTime(), FactorClimatico.temperatura, 5.0f);
         ObjectContainer db = Db4o.openFile("resources/dbejemplo.yap");
         db.store(dato1);
-//        DatoSensado proto = new DatoSensado(null, null, FactorClimatico.temperatura, null);
-//        ObjectSet result=db.queryByExample(proto);
-//        System.out.println(result.size());
-//        while(result.hasNext()) {
-//            System.out.println(result.next());
-//        }
         List<DatoSensado> datos = db.query(DatoSensado.class);
         for (DatoSensado datoSensado : datos) {
             System.out.println(datoSensado.toString());
         }
-        //assertFalse(predictor.analizar(regla, datos));
     }
 
     @Test
@@ -70,15 +64,26 @@ public class SelectorDatosTest {
         String rutabd = "resources/dbejemploDA.yap";
         DatoAlmacenado dato1;
         new File(rutabd).delete();
-        ObjectContainer db = Db4o.openFile(rutabd);
-
+        ObjectServer server1 = Db4o.openServer(rutabd, 0);
+        ObjectContainer cliente1 = server1.openClient();
+        ObjectContainer cliente2 = server1.openClient();
         for (int i = 0; i < 10000; i++) {
-            dato1 = new DatoAlmacenado(i, Calendar.getInstance().getTime(), FactorClimatico.presion, 5.0f, 3, DataSource.triangulador);
-            db.store(dato1);
+            dato1 = new DatoAlmacenado(i, Calendar.getInstance().getTime(), FactorClimatico.presion, 5.0f, 3, DataSource.terminal_remota);
+            cliente1.store(dato1);
+            dato1 = new DatoAlmacenado(i, Calendar.getInstance().getTime(), FactorClimatico.temperatura, 5.0f, 3, DataSource.terminal_remota);
+            cliente2.store(dato1);
         }
 
-        List<DatoAlmacenado> datos = db.query(DatoAlmacenado.class);
+        List<DatoAlmacenado> datos = cliente1.query(DatoAlmacenado.class);
         assertTrue(datos.size() == 10000);
-        db.close();
+        datos = cliente2.query(DatoAlmacenado.class);
+        assertTrue(datos.size() == 10000);
+        cliente1.commit();
+        cliente2.commit();
+        datos = cliente2.query(DatoAlmacenado.class);
+        assertTrue(datos.size() == 20000);
+
+        cliente1.close();
+        cliente2.close();
     }
 }
