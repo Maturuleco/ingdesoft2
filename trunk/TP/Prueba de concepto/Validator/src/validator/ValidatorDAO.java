455d3f6358d80198813180ed169581874dee41ba
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 20:35:49 +0000

    - validador

---
+++ trunk/TP/Prueba de concepto/Validator/src/validator/ValidatorDAO.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validator;
+
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectServer;
+import com.db4o.ObjectSet;
+import com.db4o.ext.DatabaseClosedException;
+import com.db4o.ext.DatabaseReadOnlyException;
+import java.util.Collection;
+import java.util.List;
+import model.DatoAlmacenado;
+
+/**
+ *
+ * @author Santiago Avendaño
+ */
+public class ValidatorDAO {
+
+    private ObjectServer server;
+
+    public ValidatorDAO(ObjectServer server) {
+        this.server = server;
+    }
+
+    public void escribirDatosValidos(Collection<DatoAlmacenado> datos) {
+        ObjectContainer cliente = server.openClient();
+        try {
+            for (DatoAlmacenado datoAlmacenado : datos) {
+                cliente.store(datoAlmacenado);
+            }
+            cliente.commit();
+        } catch (DatabaseClosedException e) {
+            System.out.println("la base que intenta ingresar se encuentra cerrada");
+            System.out.println(e.getMessage());
+        } catch (DatabaseReadOnlyException e) {
+            System.out.println("la base que intenta ingresar esta en estado read-only");
+            System.out.println(e.getMessage());
+        } finally {
+            cliente.close();
+        }
+    }
+
+
+    public void escribirDatosValidos(DatoAlmacenado dato) {
+        ObjectContainer cliente = server.openClient();
+        try {
+            cliente.store(dato);
+            cliente.commit();
+        } catch (DatabaseClosedException e) {
+            System.out.println("la base que intenta ingresar se encuentra cerrada");
+            System.out.println(e.getMessage());
+        } catch (DatabaseReadOnlyException e) {
+            System.out.println("la base que intenta ingresar esta en estado read-only");
+            System.out.println(e.getMessage());
+        } finally {
+            cliente.close();
+        }
+    }
+
+    // retorna si el dato fue guardado correctamente
+    public void escribirDatosOutlier(DatoAlmacenado dato) {
+        ObjectContainer cliente = server.openClient();
+        try {
+            cliente.store(dato);
+        } catch (DatabaseClosedException e) {
+            System.out.println("la base que intenta ingresar se encuentra cerrada");
+            System.out.println(e.getMessage());
+        } catch (DatabaseReadOnlyException e) {
+            System.out.println("la base que intenta ingresar esta en estado read-only");
+            System.out.println(e.getMessage());
+        } finally {
+            cliente.close();
+        }
+    }
+
+    public List<DatoAlmacenado> leerDatosValidos() {
+        DatoAlmacenado prototipo = new DatoAlmacenado(null, null, null, null, null, null);
+        ObjectContainer cliente = server.openClient();
+        ObjectSet<DatoAlmacenado> resultado;
+        try {
+            resultado = cliente.queryByExample(prototipo);
+        } finally {
+            cliente.close();
+        }
+        return resultado.subList(0, resultado.size() - 1);
+    }
+
+    public List<DatoAlmacenado> leerDatosValidos(DatoAlmacenado prototipo) {
+
+        ObjectContainer cliente = server.openClient();
+        ObjectSet<DatoAlmacenado> resultado;
+        try {
+            resultado = cliente.queryByExample(prototipo);
+        } finally {
+            cliente.close();
+        }
+        return resultado.subList(0, resultado.size() - 1);
+    }
+}
