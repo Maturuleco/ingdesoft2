da0e15b3f9e2fe5aac4a6dfad038fc9f6266c09d
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 18:30:30 +0000

    Agrego + cosas al repo...

--- trunk/TP/Prueba de concepto/TerminalRemota/src/TR/Initialize.java
+++ trunk/TP/Prueba de concepto/TerminalRemota/src/TR/Initialize.java
@@ -5,16 +5,77 @@
 
 package TR;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import threadcomunication.Sensor;
 
 /**
  *
  * @author matiaz
  */
 public class Initialize {
-    public static int estacionCentral = 0;
+    public static int estacionCentral;
     public static int TRid;
     private static int numeroModem;
+    private static Sensor[] sensores;
+
+    private static String getDatoFromLine(String linea) throws ParseException {
+        if (linea == null)
+            throw new ParseException("Configuration file", 0);
+        String[] partes = linea.split(":");
+        if (partes.length != 2)
+            throw new ParseException("En archivo de configuracion "+linea, 0);
+        return partes[1];
+    }
+
+    private static boolean configurar(File configFile) throws ParseException {
+
+        FileReader fr = null;
+        try {
+            if (!configFile.canRead()) {
+                return false;
+            }
+            fr = new FileReader(configFile);
+            BufferedReader br = new BufferedReader(fr);
+
+            String linea = br.readLine();
+            String dato = getDatoFromLine(linea);
+            TRid = Integer.valueOf(dato);
+
+            linea = br.readLine();
+            dato = getDatoFromLine(linea);
+            numeroModem = Integer.valueOf(dato);
+
+            linea = br.readLine();
+            dato = getDatoFromLine(linea);
+            estacionCentral = Integer.valueOf(dato);
+            
+            while (linea != null) {
+                // fr.close();
+                configFile.delete();
+                linea = br.readLine();
+            }
+            fr.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Initialize.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Initialize.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            try {
+                fr.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Initialize.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return true;
+    }
+
 
     /**
      * @param args the command line arguments
@@ -28,7 +89,7 @@ public class Initialize {
             if (! configFile.isFile())
                 System.out.println("Es necesario el archivo de configuracion\n");
             else {
-                
+                configurar(configFile);
             }
         }
         
