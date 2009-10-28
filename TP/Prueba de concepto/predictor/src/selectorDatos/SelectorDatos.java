5417f5cbf3dd60a69181fb4bae99c5b04ec7d244
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 04:09:37 +0000

    Adelante un poco del moduloq ue hace consultas a la base

--- TP/Prueba de concepto/predictor/src/selectorDatos/SelectorDatos.java
+++ TP/Prueba de concepto/predictor/src/selectorDatos/SelectorDatos.java
@@ -2,14 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package selectorDatos;
 
-import java.io.File;
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
-
+import com.db4o.ext.DatabaseClosedException;
+import com.db4o.ext.DatabaseReadOnlyException;
+import java.util.Collection;
+import java.util.List;
+import model.DatoAlmacenado;
 
 /**
  *
@@ -17,9 +19,37 @@ import com.db4o.ObjectSet;
  */
 public class SelectorDatos {
 
+    public static final String nombrebd = "resoruces/validData.yap";
 
-    public void escribirDatos(){
-        ObjectContainer db = Db4o.openFile("/resources/dbejemplo.yap");
-        db.store(1);
+    public void escribirDatos(Collection<DatoAlmacenado> datos) {
+        ObjectContainer db = Db4o.openFile(nombrebd);
+        try {
+            for (DatoAlmacenado datoAlmacenado : datos) {
+                db.store(datoAlmacenado);
+            }
+
+        } catch(DatabaseClosedException e){
+            System.out.println("la base que intenta ingresar se encuentra cerrada");
+            System.out.println(e.getMessage());
+        }catch(DatabaseReadOnlyException e){
+            System.out.println("la base que intenta ingresar esta en estado read-only");
+            System.out.println(e.getMessage());
+        }finally {
+            db.close();
+        }
+
+    }
+
+    public List<DatoAlmacenado> leerTodosLosDatos() {
+        DatoAlmacenado prototipo = new DatoAlmacenado(null, null, null,null, null, null);
+        ObjectContainer db = Db4o.openFile(nombrebd);
+        ObjectSet<DatoAlmacenado> resultado;
+        try {
+            resultado = db.queryByExample(prototipo);
+
+        } finally {
+            db.close();
+        }
+        return resultado.subList(0, resultado.size()-1);
     }
 }
