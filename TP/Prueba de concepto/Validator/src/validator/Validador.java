095bbe372737d00c48eccf105df831ff84e54a6c
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 20:35:49 +0000

    - validador

---
+++ TP/Prueba de concepto/Validator/src/validator/Validador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package validator;
+
+import model.DatoAlmacenado;
+import model.DatoSensado;
+
+/**
+ *
+ * @author Santiago Avendaño
+ */
+public class Validador {
+
+
+    public Boolean validar(DatoAlmacenado d){
+        return Boolean.TRUE;
+    }
+}
---
+++ TP/Prueba de concepto/Validator/src/validator/ValidatorManager.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package validator;
+
+import com.db4o.ObjectServer;
+import java.util.Calendar;
+import java.util.Date;
+import model.DataSource;
+import model.DatoAlmacenado;
+import model.FactorClimatico;
+
+/**
+ *
+ * @author Santiago Avendaño
+ */
+public class ValidatorManager implements Runnable{
+
+    private static final long tiempoEspera = 10000;
+    private volatile boolean keepTrying = true;
+
+    private ValidatorDAO validadorDAO = null;
+    private Validador validador = new Validador();
+
+    public ValidatorManager(ObjectServer server) {
+        validadorDAO = new ValidatorDAO(server);
+    }
+
+    @Override
+    public void run() {
+        while (keepTrying) {
+            try {
+                //TODO: completar cuando lleguen datos
+                Date timeStamp = Calendar.getInstance().getTime();
+                DatoAlmacenado dato = new DatoAlmacenado(1, timeStamp, FactorClimatico.temperatura, 20.0F, 1, DataSource.terminal_remota);
+                if (validador.validar(dato)){
+                    validadorDAO.escribirDatosValidos(dato);
+                } else {
+                    validadorDAO.escribirDatosOutlier(dato);
+                }
+                Thread.sleep(tiempoEspera);
+            } catch (InterruptedException ex) { }
+        }
+    }
+
+}
