def74f47232763b160cd0ee4065c9588cdd31486
chechus26 <chechus26@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 15:19:22 +0000

    - Genera Sensores con un factor al azar

--- trunk/TP/Prueba de concepto/SensorAndAdapterComunication/src/sensorandadaptercomunication/Main.java
+++ trunk/TP/Prueba de concepto/SensorAndAdapterComunication/src/sensorandadaptercomunication/Main.java
@@ -1,15 +1,32 @@
 package sensorandadaptercomunication;
 
+import java.io.File;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.FactorClimatico;
+
 public class Main {
 
     public static void main(String[] args) {
          for (int i = 1; i <= 3; ++i){
-            String path = "C:/Documents and Settings/Lionel Raymundi/Escritorio/sensor"+i+"/";
-            
-            Sensor s = new Sensor(path, "sensor"+i, 10*i);
-            s.start();
-            Adapter a = new Adapter(path, "sensor"+i, 20*i);
-            a.start();
+            try {
+                String path = "C:/Documents and Settings/Lionel Raymundi/Escritorio/sensor" + i + "/";
+
+                File carpeta = new File(path);
+
+                if (!carpeta.exists()) {
+                    carpeta.mkdir();
+                }
+                FactorClimatico factor = FactorClimatico.getFactor();
+
+                Sensor s = new Sensor(path, "sensor" + i, factor, 10 * i);
+                s.start();
+                Adapter a = new Adapter(path, "sensor" + i, 20 * i);
+                a.start();
+            } catch (ParseException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
             
          }
             
---
+++ trunk/TP/Prueba de concepto/SensorAndAdapterComunication/src/sensorandadaptercomunication/SensorSMS.java
@@ -0,0 +1,34 @@
+package sensorandadaptercomunication;
+
+import java.util.Date;
+
+public class SensorSMS {
+    
+    private Sensor sensor;
+
+    SensorSMS(Sensor sensor) {
+        setSensor(sensor);
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+    
+    /*
+     * Se genera un mensaje con la info necesaria.
+     */
+    public String getInfo() {
+       Date d = new Date();
+       String info = sensor.getName() + "|" +
+                     sensor.getFactor().toString() + "|" +
+                     System.currentTimeMillis() + "|" +
+                     d.getTime();
+                     
+        return info;
+    }
+
+}
