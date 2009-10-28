45367a9459ffa9a6a3794a977acacf702b26771d
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-28 04:09:37 +0000

    Adelante un poco del moduloq ue hace consultas a la base

--- trunk/TP/Prueba de concepto/predictor/test/predictorTest/SelectorDatosTest.java
+++ trunk/TP/Prueba de concepto/predictor/test/predictorTest/SelectorDatosTest.java
@@ -7,16 +7,20 @@ package predictorTest;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
-import com.db4o.ObjectSet;
+import java.io.File;
 import java.util.Calendar;
 import java.util.List;
+import model.DataSource;
+import model.DatoAlmacenado;
 import model.DatoSensado;
 import model.FactorClimatico;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -44,6 +48,7 @@ public class SelectorDatosTest {
     }
 
     @Test
+    @Ignore
     public void escribirDatosTest() {
         DatoSensado dato1 = new DatoSensado(1, Calendar.getInstance().getTime(), FactorClimatico.temperatura, 5.0f);
         ObjectContainer db = Db4o.openFile("resources/dbejemplo.yap");
@@ -61,4 +66,21 @@ public class SelectorDatosTest {
         //assertFalse(predictor.analizar(regla, datos));
     }
 
+    @Test
+    public void escribirDatosAlmacenadosTest() {
+        String rutabd = "resources/dbejemploDA.yap";
+        DatoAlmacenado dato1;
+        new File(rutabd).delete();
+        ObjectContainer db = Db4o.openFile(rutabd);
+
+        for (int i = 0; i < 10000; i++) {
+            dato1 = new DatoAlmacenado(i, Calendar.getInstance().getTime() , FactorClimatico.presion, 5.0f, 3, DataSource.triangulador);
+            db.store(dato1);
+        }
+
+        List<DatoAlmacenado> datos = db.query(DatoAlmacenado.class);
+
+        assertTrue(datos.size() == 10000);
+                db.close();
+    }
 }
