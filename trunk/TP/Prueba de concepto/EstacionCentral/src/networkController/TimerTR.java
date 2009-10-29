201c3f94a00e1811845e08e72f6c07d7304f0076
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-29 04:01:56 +0000

    Limpieza de proyectos de EC

---
+++ trunk/TP/Prueba de concepto/EstacionCentral/src/networkController/TimerTR.java
@@ -0,0 +1,93 @@
+package networkController;
+
+public class TimerTR implements Runnable {
+    private String trName;
+    private int intervalo = 1000;
+    private boolean fall = false;
+    private boolean repeticion = true;
+    private boolean enEjecucion = false;
+    private Thread thread = null;
+    private NetworkController handler = null;
+
+    // Contructor basico, usado por defecto
+    TimerTR() { }
+
+    TimerTR(String name, int tiempo,NetworkController handler ) {
+        setTrName(name);
+        setIntervalo(tiempo);
+        setHandler( handler );
+    }
+
+    // Fija el objeto que ha de ser notificado de los eventos que sucedan al timer
+    public void setHandler( NetworkController handler ) {
+        this.handler = handler;
+    }
+
+    // Fija el numero de milisegundos entre pulsos del timer
+    public void setIntervalo( int intervalo ) {
+        this.intervalo = intervalo;
+    }
+
+    /*
+    * Aqui creamos un nuevo thread para correr el Timer. Lo incializamos
+    * con "this" de forma que el metodo run() se llame inmediatamente
+    * como comience la ejecucion del thread
+    */
+     public void start() {
+        thread = new Thread( this );
+        enEjecucion = true;
+        thread.start();
+    }
+
+    public void stop() {
+        enEjecucion = false;
+        thread.stop();
+    }
+
+    public void run() {
+        repeticion = true;
+        
+        while( repeticion ){
+            // Esperamos el tiempo que nos hayan dicho en la configuracion
+            // del intervalo
+            try {
+                esperar( intervalo );
+            } catch( InterruptedException e ) {
+                return;
+            }
+            // Cuando se cumple el intervalo, avisamos a las clases que
+            // esten pendientes.
+            handler.timeout( this );
+            
+            System.out.println("        C " + getTrName());
+            timerTRFall();
+            stop();
+
+            repeticion = false;
+            enEjecucion = false;
+        }
+    }
+
+    public boolean estaCorriendo() {
+        return enEjecucion ;
+    }
+
+    private synchronized void esperar( int lapso ) throws InterruptedException {
+        thread.sleep(lapso);
+    }
+
+    public String getTrName() {
+        return trName;
+    }
+
+    public void setTrName(String trName) {
+        this.trName = trName;
+    }
+
+    public void timerTRFall(){
+        fall = true;
+    }
+    public boolean getTimerTRFall(){
+        return fall;
+    }
+}
