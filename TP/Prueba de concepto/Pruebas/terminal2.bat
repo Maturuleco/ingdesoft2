aa5acea42a5582c724b861a38c33252538d0a653
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-10-30 22:24:07 +0000

    Cambio de lugar las pruebas

---
+++ TP/Prueba de concepto/Pruebas/terminal2.bat
@@ -0,0 +1,4 @@
+start /b sensor5.bat
+start /b sensor6.bat
+start /b sensor7.bat
+java -jar ..\TerminalRemotaFinal\dist\TerminalRemotaFinal.jar ..\Configuraciones\ConfigTr2.txt
--- TP/Prueba de concepto/terminal1.bat
+++
@@ -1,3 +0,0 @@
-start /b sensor1.bat
-start /b sensor2.bat
-java -jar TerminalRemotaFinal/dist/TerminalRemotaFinal.jar Configuraciones/ConfigTr1.txt
