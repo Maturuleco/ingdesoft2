ea3c53402e78b63762b16e2ef52b8af6bb647030
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-11-04 19:02:57 +0000

    La red ahora simplemente mueve los archivos

--- trunk/TP/Prueba de concepto/red_gsm/src/red_gsm/Red.java
+++ trunk/TP/Prueba de concepto/red_gsm/src/red_gsm/Red.java
@@ -2,127 +2,138 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package red_gsm;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author tas
  */
 public class Red {
+
     public static final String path = "GSM";
+
     /**
      * @param args the command line arguments
      */
+    public static boolean moveFile(File archivoAMover, String pathDestino, String nombreFinal) {
+        File destino = new File(pathDestino, nombreFinal);
+        return archivoAMover.renameTo(destino);
+    }
+
+    public static MensajeGSM readFile(File file) {
+        FileReader fr = null;
+        MensajeGSM mensaje = null;
+        try {
+            fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String texto = br.readLine();
+
+            System.out.println("La red lee :" + texto);
+            fr.close();
+            
+            mensaje = MensajeGSM.parse(texto);
+        } catch (ParseException ex) {
+            Logger.getLogger(Red.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Red.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fr.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Red.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return mensaje;
+    }
+
     public static void main(String[] args) {
-        
+
         final int maxSize = 999999;
-        Dictionary< Integer , File > modems = new Hashtable< Integer, File >();
-        Dictionary< Integer , Integer > mensajeModems = new Hashtable< Integer, Integer >();
-        File carpeta = new File (path);
-        
-        if (!carpeta.exists())
+        Dictionary<Integer, File> modems = new Hashtable<Integer, File>();
+        Dictionary<Integer, Integer> mensajeModems = new Hashtable<Integer, Integer>();
+        File carpeta = new File(path);
+
+        if (!carpeta.exists()) {
             carpeta.mkdir();
-        
-        Boolean hayMensaje;
+        }
+
+
         MensajeGSM mensaje = null;
         int numeroDest;
         int numeroOrig;
-    
+
         // recive
         while (true) {
-            hayMensaje = false;
             numeroDest = 0;
             numeroOrig = 0;
-            while (! hayMensaje) {
-                // Recive
-                try {
-                    File[] files = carpeta.listFiles();
-                    int longitud = 0;
-                    if (files != null)
-                            longitud = files.length;
-                    for (int j = 0; j < longitud; j++) {
-                        File file = files[j];
-                        //System.out.println( getName()+ "D:" + file.getName());
-                        if(file.canRead()){
-                            FileReader fr = new FileReader(file);
-                            BufferedReader br = new BufferedReader(fr);
-                            String texto= br.readLine();
-        
-                            System.out.println("La red lee :"+texto);
-                            
-                            //Lo parseo
-                            mensaje = MensajeGSM.parse(texto);
-                            // Computo el origen
-                            numeroOrig = mensaje.getOrigen();
-                            if (modems.get(numeroOrig) == null){
-                                File dirModem = new File (path+numeroOrig);
-                                modems.put(numeroOrig, dirModem);
-                                mensajeModems.put(numeroOrig, 0);
-                            }
-                            // Me aseguro de que lo pueda mandar
-                            numeroDest = mensaje.getDestino();
-                            //Este if es agregado para poder testear más fácil
-                            if (modems.get(numeroDest) == null){
-                                File dirModem = new File (path+numeroDest);
-                                modems.put(numeroDest, dirModem);
-                                mensajeModems.put(numeroDest, 0);
-                            }
-                            // Si es mandado a mi o si conozco el destino 
-                            // ya sé que lo voy a poder mandar, lo borro
-                            if (numeroDest == 0 || modems.get(numeroDest) != null) {
-                                fr.close();
-                                file.delete();
-                                if (numeroDest != 0) {
-                                    hayMensaje = true;
-                                    break;
-                                }
-                            }
-                            fr.close();
+            String texto = "";
+            try {
+                File[] files = carpeta.listFiles();
+                int longitud = 0;
+                if (files != null) {
+                    longitud = files.length;
+                }
+
+                for (int j = 0; j < longitud; j++) {
+                    File file = files[j];
+                    //System.out.println( getName()+ "D:" + file.getName());
+                    if (file.canRead()) {
+                        mensaje = readFile(file);
+                        if (mensaje == null)
+                            break;
+                        // Computo el origen
+                        numeroOrig = mensaje.getOrigen();
+                        if (modems.get(numeroOrig) == null) {
+                            File dirModem = new File(path + numeroOrig);
+                            modems.put(numeroOrig, dirModem);
+                            mensajeModems.put(numeroOrig, 0);
                         }
 
+                        numeroDest = mensaje.getDestino();
+
+                        if (modems.get(numeroDest) == null) {
+                            File dirModem = new File(path + numeroDest);
+                            modems.put(numeroDest, dirModem);
+                            mensajeModems.put(numeroDest, 0);
+                        }
+                        // Si es mandado a mi lo borro
+
+                        if (numeroDest == 0) {
+                            file.delete();
+                        } // y si conozco el destino lo muevo
+                        else if (modems.get(numeroDest) != null) {
+                            // Creo el nombre del archivo a mover
+                            int numeroMsj = mensajeModems.get(numeroDest);
+                            String nombre = numeroOrig + "-" + numeroMsj + ".txt";
+
+                            String pathDestino = modems.get(numeroDest).getPath();
+
+                            moveFile(file, pathDestino, nombre);
+
+                            numeroMsj++;
+                            if (numeroMsj >= maxSize) {
+                                numeroMsj = 0;
+                            }
+                            mensajeModems.put(numeroDest, numeroMsj);
+                        }
                     }
-                } catch (Exception e) {
-                      e.printStackTrace(System.err);
                 }
-            }
-            
-            int numeroMsj = mensajeModems.get(numeroDest);
-            
-            // Send
-            String pathDestino = modems.get(numeroDest).getPath();
-
-            String phrase = mensaje.toString();
-
-            try {
-                pathDestino +=  "/" + numeroOrig + "-" + numeroMsj +".txt";
-                //System.out.println(getName() + " C: " +"sms"+ j +".txt");
-
-                File file = new File(pathDestino);
-                FileOutputStream fos = new FileOutputStream(file);
-
-                for (char ch : phrase.toCharArray()) {
-                    fos.write(ch);
-                }
-                fos.close();
-                System.out.println("La red escribió: "+phrase);
-                numeroMsj++;
-                if (numeroMsj >= maxSize)
-                    numeroMsj = 0;
-                mensajeModems.put(numeroDest, numeroMsj);
-
-            } catch (Exception e) {
-                      e.printStackTrace(System.err);
+            } catch  (Exception e) {
+            e.printStackTrace(System.err);
             }
         }
-    
     }
 
 }
