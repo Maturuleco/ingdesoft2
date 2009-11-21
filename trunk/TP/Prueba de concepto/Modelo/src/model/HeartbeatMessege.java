5e980099d0a2e4629df9050ae8a62ba783d056d4
santiavenda2 <santiavenda2@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-11-21 17:11:39 +0000

    Primer commit de POC2 (NC)

--- trunk/TP/Prueba de concepto/Modelo/src/model/HeartbeatMessege.java
+++ trunk/TP/Prueba de concepto/Modelo/src/model/HeartbeatMessege.java
@@ -1,19 +1,17 @@
-package networkController;
+package model;
 
 import java.util.Date;
 
-public class HeartbeatMessege {
-
-    private Integer idTR;
-    private Date trTimestamp;
+public class HeartbeatMessege extends MensajeNetworkController {
 
     private String sensorName;
     private Date sensorTimestamp;
 
     public HeartbeatMessege(Integer idTR, Date trTimestamp, String sensorName, Date sensorTimestamp){
-        this.idTR = idTR;
+        
+        setIdTR(idTR);
+        setFecha(trTimestamp);
         this.sensorName = sensorName;
-        this.trTimestamp = trTimestamp;
         this.sensorTimestamp = sensorTimestamp;       
     }
 
@@ -33,25 +31,10 @@ public class HeartbeatMessege {
         this.sensorTimestamp = sensorTimestamp;
     }
 
-    public Integer getTrName() {
-        return idTR;
-    }
-
-    public void setTrName(Integer idTR) {
-        this.idTR = idTR;
-    }
-
-    public Date getTrTimestamp() {
-        return trTimestamp;
-    }
-
-    public void setTrTimestamp(Date trTimestamp) {
-        this.trTimestamp = trTimestamp;
-    }
-
+   
     @Override
      public String toString(){
-        return " ID TR: " + this.idTR;
+        return " ID TR: " + this.getIdTR();
  //              " SENSOR NAME: " + this.sensorName +
  //              " TR TIME: " + this.trTimestamp +
  //              " SENSOR TIME: " + this.sensorTimestamp;
--- trunk/TP/Prueba de concepto/red_gsm/src/red_gsm/MensajeGSM.java
+++ trunk/TP/Prueba de concepto/red_gsm/src/red_gsm/MensajeGSM.java
@@ -1,7 +1,6 @@
 package red_gsm;
 
 import java.text.ParseException;
-import java.util.Comparator;
 import java.util.Date;
 
 public class MensajeGSM {
