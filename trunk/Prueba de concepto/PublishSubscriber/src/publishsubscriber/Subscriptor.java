f2ef087a87eb057951c1d5a89057d34ed9552a67
martaponzoni <martaponzoni@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-05 19:59:46 +0000

    suscriptor

---
+++ trunk/Prueba de concepto/PublishSubscriber/src/publishsubscriber/Subscriptor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package publishsubscriber;
+import java.util.concurrent.BlockingQueue;
+import model.*;
+
+/**
+ *
+ * @author mar
+ */
+public abstract class Subscriptor extends Thread{
+    private BlockingQueue<SubscriberMessage> salida;
+    private BlockingQueue<SubscriptionAcceptedMessage> entrada;
+
+    public Subscriptor() {
+    }
+
+    @Override
+    public void run(){
+        subscribe();
+    }
+
+    public void setEntrada(BlockingQueue<SubscriberMessage> salida) {
+        this.salida = salida;
+    }
+
+    public void setSalida (BlockingQueue<SubscriptionAcceptedMessage> entrada) {
+        this.entrada = entrada;
+    }
+
+    public void subscribe () {
+        SubscriberMessage mensaje = crearMensajeSuscripcion();
+        salida.add(mensaje);
+    }
+
+    protected abstract SubscriberMessage crearMensajeSuscripcion();
+}
---
+++ trunk/Prueba de concepto/PublishSubscriber/src/publishsubscriber/TRsFactoresSuscripcion.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package publishsubscriber;
+
+import java.util.List;
+import model.FactorClimatico;
+
+/**
+ *
+ * @author mar
+ */
+public class TRsFactoresSuscripcion extends Suscripcion{
+
+    public List<FactorClimatico> getFactores() {
+        return factores;
+    }
+
+    public void setFactores(List<FactorClimatico> factores) {
+        this.factores = factores;
+    }
+
+    public Integer getIdEC() {
+        return idEC;
+    }
+
+    public void setIdEC(Integer idEC) {
+        this.idEC = idEC;
+    }
+
+    public List<Integer> getIdsTR() {
+        return idsTR;
+    }
+
+    public void setIdsTR(List<Integer> idsTR) {
+        this.idsTR = idsTR;
+    }
+
+    Integer idEC;
+    List<Integer> idsTR;
+    List<FactorClimatico> factores;
+}
