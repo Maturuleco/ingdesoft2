1f6bf132160c72ce56c6c66b8a61c6d10c7a54d3
martaponzoni <martaponzoni@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-08 12:46:46 +0000

    suscriptor

--- trunk/Prueba de concepto/Modelo/src/model/InformationMessage.java
+++ trunk/Prueba de concepto/Modelo/src/model/InformationMessage.java
@@ -13,6 +13,22 @@ public class InformationMessage extends SuscriptorMessage{
     Integer receptor;
     InformationMessage mensaje;
 
+    public InformationMessage getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(InformationMessage mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public Integer getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(Integer receptor) {
+        this.receptor = receptor;
+    }
+
     public InformationMessage(Integer receptor, InformationMessage mensaje) {
         this.receptor = receptor;
         this.mensaje = mensaje;
