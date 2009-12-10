5e3e4545c4adea010ff13a54e0b944cf37a2ecf4
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-10 16:52:41 +0000

    Ec Comunicator terminado supuestamente (falta testear)

--- Prueba de concepto/EstacionCentral/src/EcComunicator/EcComunicator.java
+++ Prueba de concepto/EstacionCentral/src/EcComunicator/EcComunicator.java
@@ -2,24 +2,55 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package EcComunicator;
 
+import NetworkComunication.NetworkDestination;
+import SubscripcionesEc.MensajePedidoSubscripcionDatos;
+import SubscripcionesEc.MensajePedidoSubscripcionResultados;
+import SubscripcionesEc.SubscriberMessage;
+import SubscripcionesEc.SubscriptionAcceptedMessage;
 import com.thoughtworks.xstream.XStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.InformationMessage;
 
 /**
  *
  * @author tas
  */
-public class EcComunicator {
-    ControladorPuertos puertosEcs;
-    
+public class EcComunicator implements Runnable {
+
+    private static final long sleeptime = 1000;
+    private ControladorPuertos puertosEcs;
+    private Boolean serversReady = false;
+//    private Boolean ecComunicReady = false;
+    private ClienteInformacion clienteEnvioDatos;
+    private ClienteInformacion clienteEnvioResult;
+    private ClienteSubscripciones clienteSubsDatos;
+    private ClienteSubscripciones clienteSubsResult;
+    private ServerInformacion<InformationMessage> serverDatos;
+    private ServerInformacion<InformationMessage> serverResultados;
+    private ServerSubscripcones<MensajePedidoSubscripcionDatos> serverSubsDatos;
+    private ServerSubscripcones<MensajePedidoSubscripcionResultados> serverSubsResult;
+    private BlockingQueue<SubscriberMessage> entradaEnvioSubscripcionesDatos;
+//    private BlockingQueue<SubscriptionAcceptedMessage> salidaRespSubscripcionesDatos;
+    private BlockingQueue<SubscriberMessage> entradaEnvioSubscripcionesResult;
+//    private BlockingQueue<SubscriptionAcceptedMessage> salidaRespSubscripcionesResult;
+    private BlockingQueue<InformationMessage> entradaDatos;
+    private BlockingQueue<InformationMessage> entradaResult;
+
+//    private BlockingQueue<InformationMessage> salidaDatosExternos;
+//    private BlockingQueue<InformationMessage> salidaResultExternos;
+//    private BlockingQueue<SubscriberMessage> salidaSubscripExternasDatos;
+//    private BlockingQueue<SubscriptionAcceptedMessage> entradaRespSubscripExternasDatos;
+//    private BlockingQueue<SubscriberMessage> salidaSubscripExternasResult;
+//    private BlockingQueue<SubscriptionAcceptedMessage> entradaRespSubscripExternasResult;
+
     /* Idea: El EcComunicator va a tener asignados 8 puertos, 4 de envio y 4 de recepción,
      * Los 4 de envío se implementan con Clientes, y los 4 de Recepción con Servers
      * De cada tipo hay pos tipos, uno para datos y otro para información. La idea es que el 
@@ -41,14 +72,82 @@ public class EcComunicator {
      * de clientes por Ec ajena...
      * 
      * Ok, a esto le falta... :P
-    */
-    public boolean configurar(String pathPortsEcs) {
+     */
+    public EcComunicator(String pathArchivoPuertosEcs) {
+        this.clienteEnvioDatos = new ClienteInformacion();
+        this.clienteEnvioResult = new ClienteInformacion();
+        this.clienteSubsDatos = new ClienteSubscripciones();
+        this.clienteSubsResult = new ClienteSubscripciones();
+        initialize(pathArchivoPuertosEcs);
+    }
+
+    //Datos Internos :
+    public void setEntradaDatos(BlockingQueue<InformationMessage> entradaInformacion) {
+        this.entradaDatos = entradaInformacion;
+    }
+
+    public void setEntradaResult(BlockingQueue<InformationMessage> entradaResult) {
+        this.entradaResult = entradaResult;
+    }
+    // Subscripciones Internas :
+    public void setEntradaEnvioSubscripcionesDatos(BlockingQueue<SubscriberMessage> entradaEnvioSubscripcionesDatos) {
+        this.entradaEnvioSubscripcionesDatos = entradaEnvioSubscripcionesDatos;
+    }
+
+    public void setEntradaEnvioSubscripcionesResult(BlockingQueue<SubscriberMessage> entradaEnvioSubscripcionesResult) {
+        this.entradaEnvioSubscripcionesResult = entradaEnvioSubscripcionesResult;
+    }
+
+    public void setSalidaRespSubscripcionesDatos(BlockingQueue<SubscriptionAcceptedMessage> salidaRespSubsDatos) {
+        clienteSubsDatos.setSalida(salidaRespSubsDatos);
+    }
+
+    public void setSalidaRespSubscripcionesResult(BlockingQueue<SubscriptionAcceptedMessage> salidaRespSubsResult) {
+        clienteSubsResult.setSalida(salidaRespSubsResult);
+    }
+    // Datos Externos :
+    public void setSalidaDatosExternos(BlockingQueue<InformationMessage> salidaDatos) {
+        serverDatos.setSalidaDeInformacion(salidaDatos);
+    }
+
+    public void setSalidaResultExternos(BlockingQueue<InformationMessage> salidaResult) {
+        serverResultados.setSalidaDeInformacion(salidaResult);
+    }
+    // Subscripciones Externas :
+    public void setSalidaSubscripExternasDatos(BlockingQueue<SubscriberMessage> salidaSubscripExternasDatos) {
+        serverSubsDatos.setSalidaDeSubscripciones(salidaSubscripExternasDatos);
+    }
+
+    public void setEntradaRespSubscripExternasDatos(BlockingQueue<SubscriptionAcceptedMessage> entradaRespSubscripExternasDatos) {
+        serverSubsDatos.setEntradaDeRespuestasDeSubscripciones(entradaRespSubscripExternasDatos);
+    }
+
+    public void setSalidaSubscripExternasResult(BlockingQueue<SubscriberMessage> salidaSubscripExternasResult) {
+        serverSubsResult.setSalidaDeSubscripciones(salidaSubscripExternasResult);
+    }
+
+    public void setEntradaRespSubscripExternasResult(BlockingQueue<SubscriptionAcceptedMessage> entradaRespSubscripExternasResult) {
+        serverSubsResult.setEntradaDeRespuestasDeSubscripciones(entradaRespSubscripExternasResult);
+    }
+
+    public void initialize(String pathPortsEcs) {
+        if (configurar(pathPortsEcs)) {
+            Integer idEc = estacioncentral.Main.idEc;
+            this.serverDatos = new ServerInformacion<InformationMessage>(puertosEcs.getPort(idEc, TypeServerDestinyPort.envio_datos));
+            this.serverResultados = new ServerInformacion<InformationMessage>(puertosEcs.getPort(idEc, TypeServerDestinyPort.envio_resultado));
+            this.serverSubsDatos = new ServerSubscripcones<MensajePedidoSubscripcionDatos>(puertosEcs.getPort(idEc, TypeServerDestinyPort.envio_subscripcion_datos));
+            this.serverSubsResult = new ServerSubscripcones<MensajePedidoSubscripcionResultados>(puertosEcs.getPort(idEc, TypeServerDestinyPort.envio_subscripcion_resultados));
+
+            serversReady = true;
+        }
+    }
+
+    private boolean configurar(String pathPortsEcs) {
         File configFile = new File(pathPortsEcs);
         boolean res = false;
         if (!configFile.isFile() || !configFile.canRead()) {
-            System.out.println("Archivo de Puertos Inaccesible");
-        }
-        else {
+            System.out.println("\n\t####----ALERTA----####\tArchivo de Puertos Inaccesible.\n");
+        } else {
             FileInputStream fis = null;
             try {
                 configFile.setReadOnly();
@@ -70,5 +169,47 @@ public class EcComunicator {
         }
         return res;
     }
-    
+
+    public void run() {
+        while (true) {
+            if (entradaDatos.size() > 0) {
+                InformationMessage dato = entradaDatos.poll();
+                procesarDato(dato);
+            } else if (entradaResult.size() > 0) {
+                InformationMessage result = entradaResult.poll();
+                procesarResult(result);
+            } else if (entradaEnvioSubscripcionesDatos.size() > 0) {
+                SubscriberMessage subscripcion = entradaEnvioSubscripcionesDatos.poll();
+                procesarSubscripcionDatos(subscripcion);
+            } else if (entradaEnvioSubscripcionesResult.size() > 0) {
+                SubscriberMessage subscripcion = entradaEnvioSubscripcionesResult.poll();
+                procesarSubscripcionResultado(subscripcion);
+            } else {
+                try {
+                    Thread.sleep(sleeptime);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(EcComunicator.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    private void procesarDato(InformationMessage dato) {
+        NetworkDestination destino = puertosEcs.getNetworkDestination(dato.getReceptor(), TypeServerDestinyPort.envio_datos);
+        clienteEnvioDatos.send(dato, destino);
+    }
+
+    private void procesarResult(InformationMessage result) {
+        NetworkDestination destino = puertosEcs.getNetworkDestination(result.getReceptor(), TypeServerDestinyPort.envio_resultado);
+        clienteEnvioResult.send(result, destino);
+    }
+
+    private void procesarSubscripcionDatos(SubscriberMessage subscripcion) {
+        NetworkDestination destino = puertosEcs.getNetworkDestination(subscripcion.getEcProovedora(), TypeServerDestinyPort.envio_subscripcion_datos);
+        clienteSubsDatos.send(subscripcion, destino);
+    }
+
+    private void procesarSubscripcionResultado(SubscriberMessage subscripcion) {
+        NetworkDestination destino = puertosEcs.getNetworkDestination(subscripcion.getEcProovedora(), TypeServerDestinyPort.envio_subscripcion_resultados);
+    }
 }
