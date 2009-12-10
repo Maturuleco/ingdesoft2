a4b28e9d382125232e1935b4e1a464c81f666ce3
elmaildematiaz <elmaildematiaz@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-10 18:01:02 +0000

    Para que cargue el archivo de configuracion de puertos

--- Prueba de concepto/EstacionCentral/src/EcComunicator/ControladorPuertos.java
+++ Prueba de concepto/EstacionCentral/src/EcComunicator/ControladorPuertos.java
@@ -7,14 +7,15 @@ package EcComunicator;
 
 import NetworkComunication.NetworkDestination;
 import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  *
  * @author tas
  */
 public class ControladorPuertos {
-    Dictionary<Integer, Dictionary<TypeOfPort, Integer>> puertos;
-    Dictionary<Integer, String> hosts;
+    Dictionary<Integer, Dictionary<TypeOfPort, Integer>> puertos = new Hashtable<Integer, Dictionary<TypeOfPort, Integer>>();
+    Dictionary<Integer, String> hosts = new Hashtable<Integer, String>();
     
     public NetworkDestination getNetworkDestination(Integer ec, TypeServerDestinyPort tipoPuerto) {
         Integer port = null;
@@ -35,5 +36,18 @@ public class ControladorPuertos {
         if (puertosEc != null)
             port = puertosEc.get(tipoPuerto);
        return port;
+    }
+    
+    public void addHost(Integer idEc, String host) {
+        hosts.put(idEc, host);
+    }
+    
+    public void addPort(Integer idEc, TypeOfPort top, Integer port ) {
+        if (puertos.get(idEc) == null) {
+            puertos.put(idEc, new Hashtable<TypeOfPort, Integer>());
+        }
+        Dictionary<TypeOfPort, Integer> value = puertos.get(idEc);
+        value.put(top, port);
+//        puertos.put(idEc, value);
     }
 }
