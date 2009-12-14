e6fa1e0fcb6bd41fa4a53ab2290e35b74ed66f34
chechus26 <chechus26@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-14 19:15:06 +0000

    terminales bat

--- Prueba de concepto/Pruebas/terminal1.bat
+++ Prueba de concepto/Pruebas/terminal1.bat
@@ -1,5 +1,3 @@
 start /b sensor1.bat
 start /b sensor2.bat
-start /b sensor3.bat
-start /b sensor4.bat
 java -jar ..\TerminalRemotaFinal\dist\TerminalRemotaFinal.jar ..\Configuraciones\ConfigTr1.txt
--- Prueba de concepto/Pruebas/terminal2.bat
+++ Prueba de concepto/Pruebas/terminal2.bat
@@ -1,1 +1,2 @@
+start /b sensor5.bat
 java -jar ..\TerminalRemotaFinal\dist\TerminalRemotaFinal.jar ..\Configuraciones\ConfigTr2.txt
