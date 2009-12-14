4a12fd0a1eb1212a74604ea335968b1e8db6bb8a
chechus26 <chechus26@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-14 21:24:37 +0000

    poc resultado

---
+++ POC_prudente/Pruebas/terminal2.bat
@@ -0,0 +1,2 @@
+start /b sensor5.bat
+java -jar ..\TerminalRemotaFinal\dist\TerminalRemotaFinal.jar ..\Configuraciones\ConfigTr2.txt
---
+++ POC_prudente/Pruebas/terminal3.bat
@@ -0,0 +1,2 @@
+start sensor8.bat
+java -jar ..\TerminalRemotaFinal\dist\TerminalRemotaFinal.jar ..\Configuraciones\ConfigTr3.txt
