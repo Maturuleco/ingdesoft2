813a1e273a2db37857e367289a63860a446aa11d
martaponzoni <martaponzoni@d2668f7e-9ce1-11de-8fab-25195aebac49>
2009-12-14 05:23:52 +0000

    cambios para que ande con dos ecs suscribiendose mutuamente

--- trunk/Prueba de concepto/EstacionCentral/src/SubscriptorModelos/SubscriptorRequerimiento.java
+++ trunk/Prueba de concepto/EstacionCentral/src/SubscriptorModelos/SubscriptorRequerimiento.java
@@ -38,8 +38,12 @@ public class SubscriptorRequerimiento<T extends Requerimiento> extends Subscript
 
                 if( !isSubscripto(mensaje) ){
                     subscribe(mensaje);
-                    //System.out.println("[SR]Me suscribo a req de Ec: "+req.getProveedor());
+                    System.out.println("[SR]Me suscribo a req de Ec: "+req.getProveedor());
                 }
+//                else{
+//                    System.out.println("[SR] YA ESTAS SUBSCRIPTO: " + req.toString());
+//                }
+
             }
         }
         
--- trunk/Prueba de concepto/Modelo/src/SubscripcionesEc/MensajePedidoSubscripcionDatos.java
+++ trunk/Prueba de concepto/Modelo/src/SubscripcionesEc/MensajePedidoSubscripcionDatos.java
@@ -36,30 +36,37 @@ public class MensajePedidoSubscripcionDatos extends SubscriberMessage{
 
     @Override
     public boolean equals(Object obj) {
+        boolean eq = true;
         if (obj == null) {
-            return false;
+            eq =false;
         }
         if (getClass() != obj.getClass()) {
-            return false;
+            //System.out.println("distinto: " + this.getClass()+" != " +obj.getClass());
+            eq=false;
         }
         final MensajePedidoSubscripcionDatos other = (MensajePedidoSubscripcionDatos) obj;
-        if (this.timeStamp != other.timeStamp && (this.timeStamp == null || !this.timeStamp.equals(other.timeStamp))) {
-            return false;
-        }
+        
         if (this.tr != other.tr && (this.tr == null || !this.tr.equals(other.tr))) {
-            return false;
+            //System.out.println("distinto: " + this.tr+" != " +other.tr);
+            eq =false;
         }
         if (this.factorClimatico != other.factorClimatico) {
-            return false;
+            //System.out.println("distinto: " + this.factorClimatico+" != " +other.factorClimatico);
+            eq=false;
         }
-        return true;
+//        if(!eq)
+//            System.out.println("distinto: " + other.toString() +" != " +toString());
+        return eq;
     }
 
     @Override
     public int hashCode() {
-        int hash = 3;
+        int hash = 7;
+        hash = 73 * hash + (this.tr != null ? this.tr.hashCode() : 0);
+        hash = 73 * hash + this.factorClimatico.hashCode();
         return hash;
     }
+
 
  @Override
     public String toString(){
